45a5b1f40cc275b6475f4a5dcc335f30a538b87a
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2020-10-07 22:31:58 +0200

    first algo

--- src/Game/PlayerIA/OosalonPlayer.php
+++ src/Game/PlayerIA/OosalonPlayer.php
@@ -7,7 +7,7 @@ use Hackathon\Game\Result;
 /**
  * Class OosalonPlayers
  * @package Hackathon\PlayerIA
- * @author YOUR NAME HERE
+ * @author Bryan Andriamasy
  */
 class OosalonPlayer extends Player
 {
@@ -17,32 +17,21 @@ class OosalonPlayer extends Player
 
     public function getChoice()
     {
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get all the Choices          ?    $this->result->getChoicesFor($this->mySide)
-        // How to get the opponent Last Choice ?    $this->result->getChoicesFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the stats                ?    $this->result->getStats()
-        // How to get the stats for me         ?    $this->result->getStatsFor($this->mySide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // How to get the stats for the oppo   ?    $this->result->getStatsFor($this->opponentSide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the number of round      ?    $this->result->getNbRound()
-        // -------------------------------------    -----------------------------------------------------
-        // How can i display the result of each round ? $this->prettyDisplay()
-        // -------------------------------------    -----------------------------------------------------
-
-
+        $myLastChoice = $this->result->getLastChoiceFor($this->mySide);
+        $myLastScore = $this->result->getLastScoreFor($this->mySide);
+        if ($myLastScore === 0) {
+            if ($myLastChoice === parent::rockChoice()) {
+                return parent::scissorsChoice();
+            }
+            elseif ($myLastChoice === parent::paperChoice()) {
+                return parent::rockChoice();
+            }
+            else
+                return parent::paperChoice();
+        }
+        elseif ($myLastScore === 1) {
+            return parent::paperChoice();
+        }
         return parent::rockChoice();
-
     }
 };
